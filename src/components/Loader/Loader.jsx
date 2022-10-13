592e75d82916314f643dcd6e6e41d2606af07360
Daniil-Kachura <ka4urad@gmail.com>
2022-10-13 18:59:35 +0300

    image-finder

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,19 @@
 import s from './Loader.module.css';
+import { ColorRing } from 'react-loader-spinner';
+import PropTypes from 'prop-types';
 
-const Loader = () => (
-  <div className={s.ldsEllipsis}>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-);
+
+const Loader = ({ color, size }) => {
+  return (
+    <div className={s.ldsEllipsis}>
+      <ColorRing color={color} size={size} />
+    </div>
+  );
+};
+Loader.propTypes = {
+  color: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+};
+
 
 export default Loader;
